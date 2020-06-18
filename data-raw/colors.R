38a5be68e8bf11f04839f094f2e14735f4df00a1
Matt Garber <mattgarber4@gmail.com>
2020-06-18 00:13:29 -0400

    color fixes

--- data-raw/colors.R
+++ data-raw/colors.R
@@ -6,12 +6,12 @@ demBlueWhite <- "#E2F6FF"
 gopRed <- "#FE6A59"
 gopRedAlt <- "#AF1201"
 gopRedLight <- "#FFD4D6"
-gopRedWhite <- "FFEEF0"
+gopRedWhite <- "#FFEEF0"
 
 indGreen <- "#70D670"
 indGreenAlt <- "#225A1F"
-indGreenLight <- "C4EDC2"
-indGreenWhite <- "E6F8E6"
+indGreenLight <- "#C4EDC2"
+indGreenWhite <- "#E6F8E6"
 usethis::use_data(demBlue, demBlueAlt,
                   demBlueLight, demBlueWhite,
                   gopRed, gopRedAlt,
