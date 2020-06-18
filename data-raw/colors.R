ef216df76ee0608b45a3266f705eec8d2b8c43b4
Matt Garber <mattgarber4@gmail.com>
2020-06-17 22:01:01 -0400

    color palette adjustments

--- data-raw/colors.R
+++ data-raw/colors.R
@@ -1,4 +1,10 @@
 demBlue <- "#2AA1EC"
 gopRed <- "#FE6A59"
-indGreen <- "#F8F284"
-usethis::use_data(demBlue, gopRed, indGreen, overwrite = T)
+indGreen <- "#70D670"
+demBlueAlt <- "#0B507B"
+gopRedAlt <- "#AF1201"
+indGreenAlt <- "#225A1F"
+usethis::use_data(demBlue, demBlueAlt,
+                  gopRed, gopRedAlt,
+                  indGreen, indGreenAlt,
+                  overwrite = T)
