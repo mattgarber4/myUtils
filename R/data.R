fcaded8c88272a63bc1c2fb795d9ac45d1df1573
Matt Garber <mattgarber4@gmail.com>
2020-06-17 23:46:44 -0400

    full color palette

---
+++ R/data.R
@@ -0,0 +1,29 @@
+#' @rdname color-palette
+"demBlue"
+#' @rdname color-palette
+"demBlueAlt"
+#' @rdname color-palette
+"demBlueLight"
+#' @rdname color-palette
+"demBlueWhite"
+#' @rdname color-palette
+"gopRed"
+#' @rdname color-palette
+"gopRedAlt"
+#' @rdname color-palette
+"gopRedLight"
+#' @rdname color-palette
+"gopRedWhite"
+#' @rdname color-palette
+"indGreen"
+#' @rdname color-palette
+"indGreenAlt"
+#' @rdname color-palette
+"indGreenLight"
+#' @rdname color-palette
+"indGreenWhite"
+
+#' @title Color Palette
+#' @name color-palette
+#' @description Election graphics color palette including primary and alternate colors for democrats, republicants, and independents. Use \code{demBlueXXX} for democrats, \code{gopRedXXX} for republicans, and \code{indGreenXXX} for others. Primary colors are \code{demBlue}, \code{gopRed}, and \code{indGreen}. Darker alternate colors are given by the suffix \code{Alt}. Lighter accent colors are given by the suffix \code{Light}. Off-whites are given by the suffix \code{White}.
+NULL
--- data-raw/colors.R
+++ data-raw/colors.R
@@ -1,10 +1,21 @@
 demBlue <- "#2AA1EC"
-gopRed <- "#FE6A59"
-indGreen <- "#70D670"
 demBlueAlt <- "#0B507B"
+demBlueLight <- "#B6E7FF"
+demBlueWhite <- "#E2F6FF"
+
+gopRed <- "#FE6A59"
 gopRedAlt <- "#AF1201"
+gopRedLight <- "#FFD4D6"
+gopRedWhite <- "FFEEF0"
+
+indGreen <- "#70D670"
 indGreenAlt <- "#225A1F"
+indGreenLight <- "C4EDC2"
+indGreenWhite <- "E6F8E6"
 usethis::use_data(demBlue, demBlueAlt,
+                  demBlueLight, demBlueWhite,
                   gopRed, gopRedAlt,
+                  gopRedLight, gopRedWhite,
                   indGreen, indGreenAlt,
+                  indGreenLight, indGreenWhite,
                   overwrite = T)
